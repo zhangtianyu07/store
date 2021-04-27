35d03ff7b5544d829f9d11c59cc09b2c21bf0372
“tianyu” <930447165@qq.com>
2021-04-27 20:00:24 +0800

    第五次任务

--- day05/交互买卖.py
+++ day05/交互买卖.py
@@ -1,4 +1,5 @@
 # coding=gbk
+import random
 #商品
 shop = [
     ["黑鹰战机",1500], #0
@@ -8,7 +9,7 @@ shop = [
     ["自爆卡车",1500],
     ["雇佣兵",200],
     ["猎犬",100],
-    ["采矿车",1300],
+    ["采矿车",1200],
     ["恐怖机器人",500],
     ["尤里",800],     #9
 ]
@@ -16,9 +17,22 @@ shop = [
 #资金
 money = input("请输入您的资金：")
 money = int(money)
-
+money1 = money
 #购物车
 shopcar = []
+yhq = random.randint(1,31)
+print(yhq)
+print("---------优惠券种类---------")
+print("1~10、 10张采矿车优惠券：满600减300\n11~30、 20张天启坦克优惠券：半折甩卖")
+print("---------------------------")
+kc = 0
+tqtk = 0
+
+if 1 <= yhq <= 10:
+    shop[7][1] = shop[7][1] / 600 * 300
+
+else:
+    shop[1][1] = shop[1][1] / 2
 
 while True:
     for index,value in enumerate(shop):
@@ -30,13 +44,73 @@ while True:
     if num.isdigit():
         num = int(num)
         if 0 <= num < len(shop):
-            if money >= shop[num][1]:
-                money = money - shop[num][1]
-                shopcar.append(shop[num])
-                print("购买成功！！！")
-                print("您的当前余额为：",money)
+            if num == 7:
+                kc = kc + 1
+                if kc < 2:
+                    if money >= shop[num][1]:
+                        money = money - shop[num][1]
+                        shopcar.append(shop[num])
+                        print("购买成功！！！")
+                        print("您的当前余额为：",money)
+                        shop[7][1] = shop[7][1] * 2
+                    else:
+                        print("对不起，您的资金不足！！！")
+                # elif kc == 2:
+                #     # shop[7][1] = shop[7][1] * 2
+                #     if money >= shop[num][1]:
+                #         money = money - shop[num][1]
+                #         shopcar.append(shop[num])
+                #
+                #         print("购买成功！！！")
+                #         print("您的当前余额为：",money)
+                #     else:
+                #         print("对不起，您的资金不足！！！")
+
+                else:
+                    if money >= shop[num][1]:
+                        money = money - shop[num][1]
+                        shopcar.append(shop[num])
+                        print("购买成功！！！")
+                        print("您的当前余额为：",money)
+                    else:
+                        print("对不起，您的资金不足！！！")
+            elif num == 1:
+                tqtk = tqtk + 1
+                if tqtk < 2:
+                    if money >= shop[num][1]:
+                        money = money - shop[num][1]
+                        shopcar.append(shop[num])
+                        print("购买成功！！！")
+                        print("您的当前余额为：",money)
+                        shop[1][1] = shop[1][1] * 2
+                    else:
+                        print("对不起，您的资金不足！！！")
+                # elif tqtk == 2:
+                #     # shop[1][1] = shop[1][1] * 2
+                #     if money >= shop[num][1]:
+                #         money = money - shop[num][1]
+                #         shopcar.append(shop[num])
+                #
+                #         print("购买成功！！！")
+                #         print("您的当前余额为：",money)
+                #     else:
+                #         print("对不起，您的资金不足！！！")
+                else:
+                    if money >= shop[num][1]:
+                        money = money - shop[num][1]
+                        shopcar.append(shop[num])
+                        print("购买成功！！！")
+                        print("您的当前余额为：",money)
+                    else:
+                        print("对不起，您的资金不足！！！")
             else:
-                print("对不起，您的资金不足！！！")
+                if money >= shop[num][1]:
+                    money = money - shop[num][1]
+                    shopcar.append(shop[num])
+                    print("购买成功！！！")
+                    print("您的当前余额为：",money)
+                else:
+                    print("对不起，您的资金不足！！！")
         else:
             print("该商品不存在！！！请重新输入")
 
@@ -52,6 +126,7 @@ print("---------购物小票详情----------")
 for index,value in enumerate(shopcar):
     print(index,"    ",value)
 print("您的当前余额为：",money)
+print("您本次消费累计积分：",(money1 - money)/10)
 print("【欢迎下次光临！！！】")
 
 
